44dbc9d9efa87094b3e9f87e01b3b6cc9f832fd2
Gabriel Frasson <bielfc27@gmail.com>
2022-01-04 14:56:35 -0300

    Classe Loan com getters e setters

--- alexandria-library/src/main/java/br/oo/alexandria/library/Loan.java
+++ alexandria-library/src/main/java/br/oo/alexandria/library/Loan.java
@@ -4,9 +4,42 @@ import java.util.*;
 
 public class Loan {
     
-    private int idUser;
-    private int idExemplary;
-    private Date lentDate;
+    private final int ID_USER;
+    private final int ID_EXEMPLARY;
+    private final Date LENT_DATE;
     private Date devolutionDate;
-        
+
+    public Loan(int idUser, int idExemplary, Date lentDate) {
+        this.ID_USER = idUser;
+        this.ID_EXEMPLARY = idExemplary;
+        this.LENT_DATE = lentDate;
+    }
+    
+    // -------------- Getters --------------
+
+    public int getIdUser() {
+        return this.ID_USER;
+    }
+
+    public int getIdExemplary() {
+        return this.ID_EXEMPLARY;
+    }
+
+    public Date getLentDate() {
+        return this.LENT_DATE;
+    }
+
+    public Date getDevolutionDate() {
+        return this.devolutionDate;
+    }
+
+    // -------------- Setters --------------
+
+    public void setDevolutionDate(Date devolutionDate) throws Exception {
+        if (devolutionDate.compareTo(this.LENT_DATE) >= 0) {
+            this.devolutionDate = devolutionDate;
+        } else {
+            throw new Exception("A data de devolução precisa ser mais recente que a data de empréstimo");
+        }
+    }
 }
