7a1ec6cb76a89d9fbdf18832402975f635a9d3cc
isa56 <isadoraadriana@gmail.com>
2022-02-12 19:39:11 -0300

    Fazendo Controller de adicionar livros

--- alexandria-library/src/main/java/br/oo/alexandria/library/controllers/BookCreate.java
+++ alexandria-library/src/main/java/br/oo/alexandria/library/controllers/BookCreate.java
@@ -1,24 +1,36 @@
 package br.oo.alexandria.library.controllers;
 
-import br.oo.alexandria.library.views.Screen;
+import br.oo.alexandria.library.models.Book;
+import br.oo.alexandria.library.views.AddBookScreen;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.DefaultListModel;
 
 public class BookCreate implements ActionListener {
 
-    Screen screen;
+    AddBookScreen screen;
 
-    public BookCreate(Screen screen) {
+    public BookCreate(AddBookScreen screen) {
         this.screen = screen;
     }
-    
-    
     
     @Override
     public void actionPerformed(ActionEvent e) {
         
-//        DefaultListModel<Book> model = 
+        DefaultListModel<Book> model = (DefaultListModel<Book>) screen.getBookList().getModel();
+        
+        model.addElement(
+            new Book(
+                screen.getNameField().getText(),
+                screen.getAuthorField().getText(),
+                screen.getEditorField().getText(),
+                Integer.parseInt(screen.getReleaseYearField().getText()),
+                (screen.getGenreField().getText())
+            )        
+        );
+        
+        screen.getBookList().setModel(model);
+        screen.getFrame().repaint();
         
     }
 
