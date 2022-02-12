c222d2a2d52d92383c1f178b63b5b3f21a39fc9b
isa56 <isadoraadriana@gmail.com>
2022-02-12 19:23:17 -0300

    Reescrevendo JSON para listas

--- alexandria-library/src/main/java/br/oo/alexandria/library/views/Screen.java
+++ alexandria-library/src/main/java/br/oo/alexandria/library/views/Screen.java
@@ -1,16 +1,58 @@
 package br.oo.alexandria.library.views;
 
+import br.oo.alexandria.library.models.Book;
+import br.oo.alexandria.library.models.Employee;
+import br.oo.alexandria.library.models.LibraryUser;
+import br.oo.alexandria.library.models.Manager;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 public abstract class Screen {
-    
+
+    private static JList<Book> bookList;
+    private static JList<LibraryUser> libraryUsersList;
+    private static JList<Employee> employeeList;
+    private static JList<Manager> managerList;
+
     private JFrame frame;
     private JPanel mainPanel;
 
     public Screen(String frameName) {
         this.frame = new JFrame(frameName);
         this.mainPanel = new JPanel();
+    }
+
+    public JList<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(JList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public JList<LibraryUser> getLibraryUsersList() {
+        return libraryUsersList;
+    }
+
+    public void setLibraryUsersList(JList<LibraryUser> libraryUsersList) {
+        this.libraryUsersList = libraryUsersList;
+    }
+
+    public JList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(JList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public JList<Manager> getManagerList() {
+        return managerList;
+    }
+
+    public void setManagerList(JList<Manager> managerList) {
+        this.managerList = managerList;
     }
 
     public JFrame getFrame() {
@@ -28,8 +70,10 @@ public abstract class Screen {
     public void setMainPanel(JPanel mainPanel) {
         this.mainPanel = mainPanel;
     }
-    
-    
-    
-}
 
+    public static void main(String[] args) {
+        LoginScreen screen = new LoginScreen();
+
+    }
+
+}
